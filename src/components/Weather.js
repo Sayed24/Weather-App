886239a202255cc2b40e7201c45c275d52b35686
Sayed Sadat <SadatSayed92@gmail.com>
2021-12-29 22:15:03 -0800

    Setting up APIs

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { CropFree } from '@material-ui/icons';
+import React, { useState } from 'react';
+import { Container } from '@material-ui/core';
+import { WeatherInput } from './WeatherInput';
 
 export const Weather = () => {
+
+    const [query, setQuery] = useState("");
+    const [data, setData] = useState(null);
+
+    const API_KEY = '9360eee2d1781c9b0102317aaf001056';
+    const BASE_URL = 'http://api.weatherstack.com/current';
+
     return (
-        <div>
-            <h2>Weather App</h2>
-        </div>
-    )
-}
+        <Container>
+            <WeatherInput
+                data={data}
+                setData={setData}
+                apiKey={API_KEY}
+                baseUrl={BASE_URL}
+                query={query}
+                setQuery={setQuery}
+            />
+        </Container>
+    );
+};
---
+++ src/components/WeatherInput.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const WeatherInput = () => {
+    return (
+        <div>
+            <h2>Weather Input</h2>
+        </div>
+    )
+}
